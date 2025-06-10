43a45970fb050da837d8415efca87653ea95e8fb
Shinkhal <shinkhalsinha@gmail.com>
2025-06-10 22:17:39 +0530

    version 2.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import React, {useState} from 'react'
-import Navbar from './components/Navbar/Navbar'
+import  {useState} from 'react'
+import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import Verify from './pages/Verify/Verify'
-import MyOrders from './pages/MyOrders/MyOrders'
-import QueryForm from './pages/Contact/Contact'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Footer from './components/Footer'
+import LoginPopup from './components/LoginPopup'
+import Verify from './pages/Verify'
+import QueryForm from './pages/Contact'
+import MenuPage from './pages/Menu'
+import ProfilePage from './pages/ProfilePage'
+import MyOrders from './pages/MyOrders'
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const [showLogin,setShowLogin] = useState(false);
@@ -17,21 +20,31 @@ const App = () => {
     <>
     {showLogin?<LoginPopup setShowLogin={setShowLogin} ></LoginPopup>:<></>}
     <div className='app' >
-    <ToastContainer position="top-right" autoClose={3000} />
+    <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Navbar setShowLogin={setShowLogin} ></Navbar>
 
       <Routes>
-        < Route path='/' element={<Home />} />
-        < Route path='/cart' element={<Cart />} />
-        < Route path='/order' element={<PlaceOrder />} />
-        < Route path='/verify' element={<Verify />} />
-        <Route path="/myorders" element={<MyOrders/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+        <Route path='/verify' element={<Verify />} />
         <Route path="/contact" element={<QueryForm/>}/>
+        <Route path="/menu" element={<MenuPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/myorders" element={<MyOrders/>}/>
       </Routes>
     </div>
-    <Footer>
-
-    </Footer>
+    <Footer/>
     </>
   )
 }
