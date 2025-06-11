0f4dfb6a01db6f08649697d518b9cd1a231d65bc
Shinkhal <shinkhalsinha@gmail.com>
2025-06-11 10:53:50 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    fs: {
+      strict: false,
+    }
+  },
+  // Ensures 404s are redirected to index.html
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
